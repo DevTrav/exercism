232aa97054dd11f85f96caaff3255bdc1cefcb0a
Travers La Ville <traverslaville@traverss-mbp.lan>
2023-08-08 18:39:51 -0500

    Initial Commit

---
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -0,0 +1,29 @@
+
+//1. map [String] -> [Number]
+//2. join [Number] -> String
+//3.slice String -> String
+//4. parse String -> Number
+
+//Value :: [String] -> Number
+
+export const value = colorValues => {
+  return colorValues.map(colorValue => COLORS.indexOf(colorValue));
+};
+
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
+
+
+
+
+
